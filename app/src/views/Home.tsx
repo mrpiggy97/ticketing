69fd8256f9115f32ef0ef99fe4b0ef597cd437af
mrpiggy97 <fabyjesusrivas10@gmail.com>
2023-01-06 04:26:42 +0000

    added Home view and css

---
+++ app/src/views/Home.tsx
@@ -0,0 +1,12 @@
+import { JSX } from "solid-js/jsx-runtime";
+
+import "./css/Home.css"
+
+export default function Home() : JSX.Element{
+    return(
+        <div id="Home">
+            <h2 id="app-title">CyberTicket</h2>
+            <h1 id="add-app">App+</h1>
+        </div>
+    )
+}

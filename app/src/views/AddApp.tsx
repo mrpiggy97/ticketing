be395e2fecadd39e5502644ae9bc9fba48e1b136
mrpiggy97 <fabyjesusrivas10@gmail.com>
2023-01-09 04:25:16 +0000

    added more style

--- app/src/views/AddApp.tsx
+++ app/src/views/AddApp.tsx
@@ -1,15 +1,29 @@
 import { JSX } from "solid-js";
+import { useNavigate } from "@solidjs/router";
+import { apps,setApps,App } from "../store/state";
 
 import "./css/AddApp.css"
 
 export default function AddApp() : JSX.Element{
+    let navigator = useNavigate()
+    let createNewApp = (e : Event) => {
+        e.preventDefault()
+        let appNameInput = document.getElementById("new-app-name") as HTMLInputElement
+        let appName = appNameInput.value
+        let appDescriptionInput = document.getElementById("new-app-description") as HTMLInputElement
+        let appDescription = appDescriptionInput.value
+        let newApp : App = new App(appName,appDescription,2)
+        setApps([...apps,newApp])
+        navigator("/")
+    }
     return(
         <div id="new-app">
-            <form id="app-form">
-            <label class="app-name-label" for="app-name">app name</label>
-            <input type="text" class="app-name-input" maxLength={50}/>
-            <label class="app-name-label" for="app-description">app description</label>
-            <input type="text" class="app-name-input" maxLength={200}/> 
+            <form id="app-form" onsubmit={createNewApp}>
+                <label class="app-name-label" for="app-name">app name</label>
+                <input type="text" id="new-app-name" class="app-name-input" maxLength={50}/>
+                <label class="app-name-label" for="app-description">app description</label>
+                <input type="text" id="new-app-description" class="app-name-input" maxLength={200}/>
+                <button class="app-name-input-button">add</button>
             </form>
         </div>
     )

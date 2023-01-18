21050c5f588e3210e665574136c99824175b5002
mrpiggy97 <fabyjesusrivas10@gmail.com>
2023-01-18 03:11:34 +0000

    added better colors and finally fixed width overflow in mobile, added conditional render for home component

--- app/src/views/Home.tsx
+++ app/src/views/Home.tsx
@@ -1,9 +1,21 @@
-import { JSX } from "solid-js/jsx-runtime";
+import { JSX, Switch, Match, createSignal } from "solid-js";
 
 import HomeMobile from "../components/HomeMobile";
 import HomeDesktop from "../components/HomeDesktop";
 
 export default function Home() : JSX.Element{
-    let windowWidth : number = window.innerWidth
-    return windowWidth >= 1000 ? <HomeDesktop/> : <HomeMobile/>
+    let [width,setWidth] = createSignal(window.innerWidth)
+    window.addEventListener("resize", () => {
+        setWidth(window.innerWidth)
+    })
+    return(
+        <Switch>
+            <Match when={width() >= 1000}>
+                <HomeDesktop/>
+            </Match>
+            <Match when={width() < 1000}>
+                <HomeMobile/>
+            </Match>
+        </Switch>
+    )
 }
